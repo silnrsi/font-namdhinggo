c0f99590baac3e770d301de4aa75cfd2059fd8b8
Bobby de Vos <bobby_devos@sil.org>
2019-09-30 17:59:49 -0600

    Improve glyph positioning
    New APs were added, some by copying the position from other glyphs,
    others by using the manually specified position in the FEAX file.
    Once this was done, all the manually specified position rules
    could be expressed as much shorter FEAX, which also included the new
    conjuncts that were added from Unicode 7. Test data for all of this
    was also added.

---
+++ tools/anchors.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+from ranges import *
+from fontParts.world import *
+import sys
+
+# Open UFO
+ufo = sys.argv[1]
+font = OpenFont(ufo)
+print(f'Copy anchors for {ufo}')
+
+# Modify UFO
+
+# The last two consonants, U+191D and U+191E, already have the needed APs.
+for codepoint in list(range(0x1900, 0x191C + 1)):
+    consonant_name = f'uni{codepoint:X}'
+    consonant = font[consonant_name]
+    for anchor in consonant.anchors:
+        if anchor.name == 'K':
+
+            # consonant + Ukar
+            consonant_u_name = f'uni{codepoint:X}{0x1922:X}'
+            consonant_u = font[consonant_u_name]
+            consonant_u.appendAnchor('K', (anchor.x, anchor.y))
+
+            # consonant + Ra + Ukar
+            consonant_ra_u_name = f'uni{codepoint:X}{0x192A:X}{0x1922:X}'
+            if consonant_ra_u_name in font:
+                consonant_ra_u = font[consonant_ra_u_name]
+                consonant_ra_u.appendAnchor('K', (anchor.x, anchor.y))
+
+# One AP did not make it over from the old sources for unknown reasons
+omatra = font['uni1928']
+omatra.appendAnchor('K', (-150, 1455))
+
+# Not sure what the situation is with the need for these APs
+umatra = font['uni1922']
+ya = font['uni1929']
+wa = font['uni192B']
+umatra.appendAnchor('_G', (-150, -15))
+ya.appendAnchor('G', (-135, -40))
+wa.appendAnchor('G', (-135, -40))
+
+# Save UFO
+font.changed()
+font.save()
+font.close()
---
+++ tools/auto-combos.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+from ranges import *
+import sys
+
+
+with open(sys.argv[1], 'w') as output:
+
+    output.write('Consonants with vowels or Kemphreng\n')
+    for c in consonants:
+        clusters = list()
+        for m in matras + [k]:
+            cluster = c + m
+            clusters.append(cluster)
+        line = ' '.join(clusters)
+        output.write(line + '\n')
+
+    output.write('Consonants with vowels and kemphreng\n')
+    for c in consonants:
+        clusters = list()
+        for m in matras:
+            cluster = c + m + k
+            clusters.append(cluster)
+        line = ' '.join(clusters)
+        output.write(line + '\n')
+
+    output.write('Consonants with glide + Ukar + Kemphreng\n')
+    for c in consonants:
+        clusters = list()
+        for g in glides:
+            cluster = c + g + ukar + k
+            clusters.append(cluster)
+        line = ' '.join(clusters)
+        output.write(line + '\n')
+
+    output.write('Consonants with Vowels + Kemphreng + small final\n')
+    for c in consonants:
+        clusters = list()
+        for m in matras:
+            for f in finals:
+                cluster = c + m + k + f
+                clusters.append(cluster)
+        line = ' '.join(clusters)
+        output.write(line + '\n')
